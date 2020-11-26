e99880c4575daaa40e6137154d3892ce4331ce72
ernesto <ernesto002@icloud.com>
2020-11-26 09:31:18 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+    def roster
+      @roster
+    end
+  
+    def add_student(name, grade)
+     if @roster[grade] !=nil
+        @roster[grade] << name
+     else
+        @roster[grade] = [name]
+     end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+    end
+end
+      
+    
+
+school = School.new("Bayside Hogh School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.roster
+school.grade(9)
+school.roster
+school.sort
